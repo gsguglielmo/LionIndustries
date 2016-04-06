b38df1fabde50ee8ae7389c4f85045e31349382a
gsguglielmo <gsguglielmo2@gmail.com>
2016-04-06 18:09:27 +0200

    Uredil datoteke

---
+++ Html/php/credentials.php
@@ -0,0 +1,9 @@
+<?PHP
+	define("MYSQL_HOSTNAME", "localhost");
+	define("MYSQL_USERNAME", "inebriator");
+	define("MYSQL_PASSWORD", "HaSW3DTEKxtZ6mTG");
+	define("MYSQL_DATABASE", "inebriator");
+	define("MAIN_DATABASE","Inebriator");
+	define("MACHINE_DATABASE", "inebriator");
+	define("ADMIN_CODE", 7154);
+?>
